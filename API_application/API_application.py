d77bb673bee160988e88f0364ec6986fc5386648
sameyr <ss1366t@gre.ac.uk>
2024-02-23 11:15:12 +0000

    data saved in proper formatt

--- API_application/API_application.py
+++ API_application/API_application.py
@@ -1,7 +1,17 @@
 import requests
+import pandas as pd
 
+file_path='data.csv'
 url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=GBP&apikey=2HQRY5N3I5MPMIDO'
 r = requests.get(url)
 data = r.json()
 
-print(data)
+time_series = data.get('Time Series (Digital Currency Daily)')
+
+#converting csv to dataframe
+df = pd.DataFrame(time_series).T
+
+# Save the DataFrame to a CSV file
+df.to_csv('data.csv', index=True)
+
+print("Data saved to data.csv")
